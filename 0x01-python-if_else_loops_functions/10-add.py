bedf7804db8cc44e83351e4a03626dd917775aaf
Abubakar Ibrahim <ibsadiq@live.com>
2022-11-30 05:31:59 +0100

    if else

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def add(a, b):
+    """This function adds two numbers (a, b) and returns the result."""
+    return a + b
+    
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """This function returns the result of a raised to the power of b."""
+    return a ** b
+    
